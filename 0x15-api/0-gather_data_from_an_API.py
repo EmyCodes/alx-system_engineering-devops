0c43c648c78281ef1111a16340009635cc277fb1
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-04-24 16:52:25 +0000

    0-gather_data_from_an_API.py formatted to pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress """
+"""
+Checks student output for returning info from REST API
+"""
+
 import requests
 import sys
 
