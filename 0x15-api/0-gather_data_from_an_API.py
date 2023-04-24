2c5c5e6fae9a816a5588b330eb75a15f4be5bf04
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-04-24 17:42:52 +0000

    0-gather_data_from_an_API.py formatted to pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,24 +9,24 @@ if __name__ == "__main__":
 
     # Getting employee details: employee id, employee response, task response
     employeeId = sys.argv[1]
-    employeeResponse = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employeeId))
-    tasksResponse = requests.get('https://jsonplaceholder.typicode.com/todos?UserId={}'.format(employeeId))
+    users_url = 'https://jsonplaceholder.typicode.com/users/{}'
+    todos_url = 'https://jsonplaceholder.typicode.com/todos?UserId={}'
+    employeeResponse = requests.get(users_url.format(employeeId))
+    tasksResponse = requests.get(todos_url.format(employeeId))
 
     # parsing JSON response into Python dictionaries
     employeeData = employeeResponse.json()
     tasksData = tasksResponse.json()
     # Getting length of total tasks and total total data
     totalTask = len(tasksData)
-    # Getting length of total tasks and total total data 
-    totalData = len(tasksData)
-    # Getting length of total tasks and total total data 
-    totalData = len(tasksData)
+    # Getting length of total tasks and total total data
+    totalTask = len(tasksData)
     taskCompleted = [task for task in tasksData if task['completed']]
-    numOftaskCompleted = len(taskCompleted)
+    completedTask = len(taskCompleted)
 
     # display employed name and TODO list progress
-    employeeName = employeeData['name']
-    print("Emloyee {} is done with tasks({}/{})".format(employeeName, numOftaskCompleted, totalData))
+    name = employeeData['name']
+    print(f"Emloyee {name} is done with tasks({completedTask}/{totalTask})")
 
     # display the Title of tasks completed
     for task in tasksData:
