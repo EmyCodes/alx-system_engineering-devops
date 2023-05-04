3c7b9f6648019f64f7f6ee8f23c4ef9b7ed0910a
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-05-04 11:49:19 +0000

    documentation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import requests
-import sys
-
 """
 A Python script that, using the REST API,
 returns information about the TODO list progress
 for a given employee ID.
 """
+import requests
+import sys
+
 
 if __name__ == "__main__":
     # Gets Employee information
