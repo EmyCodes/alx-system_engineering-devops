4f1dbc87949077c2853b814be34eef7f0cc62ca0
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-05-04 13:01:22 +0000

    some variable names changed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,14 +16,14 @@ if __name__ == "__main__":
     # Gets Employee information
     user_id = int(sys.argv[1])
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    response1 = requests.get(user_url).json()
-    EMPLOYEE_NAME = response1.get("name")
+    user_response = requests.get(user_url).json()
+    EMPLOYEE_NAME = user_response.get("name")
 
     # Get number of tasks done
     todo_url = "https://jsonplaceholder.typicode.com/todos"
-    response2 = requests.get(todo_url).json()
+    todos_response = requests.get(todo_url).json()
     TOTAL_NUMBER_OF_TASKS = [
-        dict_ for dict_ in response2 if dict_.get("userId") == user_id
+        dict_ for dict_ in todos_response if dict_.get("userId") == user_id
     ]
     NUMBER_OF_DONE_TASKS = [
         done for done in TOTAL_NUMBER_OF_TASKS if done.get("completed")
