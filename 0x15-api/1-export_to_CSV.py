0e72860574f9dda3420e2ca0edf18546781911ed
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-05-04 14:33:30 +0000

    PEP8 complaint satisfied

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,35 +13,30 @@ import sys
 if __name__ == "__main__":
     """
     Retrieves the TODO list progress for a given employee ID
-    and prints the employee's name and the number of completed tasks.
+    and exports the data to a CSV file.
     """
-    # Gets Employee information
+    # Get employee information
     user_id = int(sys.argv[1])
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
     user_response = requests.get(user_url).json()
     username = user_response.get("username")
-    # print(username)
 
-    # Get number of tasks done
+    # Get tasks information
     todo_url = "https://jsonplaceholder.typicode.com/todos"
     todos_response = requests.get(todo_url).json()
 
-    output = [
+    # Filter tasks owned by the employee
+    tasks = [
+        [user_id, username, task.get("completed"), task.get("title")]
+        for task in todos_response
+        if task.get("userId") == user_id
+    ]
 
-            [
-                user_id,
-                username,
-                task.get("completed"),
-                task.get("title"),
-            ]
-            for task in todos_response
-        ]
-    
-
-    # print(output)
-
-    
-    with open("{}.csv".format(str(user_id)), "w", encoding="utf-8") as file_:
-        writer = csv.writer(file_, quoting=csv.QUOTE_ALL)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        writer.writerows(output)
+    # Export data to CSV file
+    filename = "{}.csv".format(user_id)
+    with open(filename, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow([
+            "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"
+        ])
+        writer.writerows(tasks)
